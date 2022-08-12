0b6cbbc77b358ec5651a4e045d7ae264689e7c17
abhishekdv1 <84401375+abhishekdv1@users.noreply.github.com>
2022-08-13 00:45:25 +0530

    Update gk&dw_questions.dart

--- gk&dw_questions.dart
+++ gk&dw_questions.dart
@@ -21,13 +21,13 @@ class Topic {
     questList.shuffle();
     int score = 0;
     for (var quest in questList) {
-      print("\nQ. ${quest.question}");
-      print("Options :");
+      print("\nQ. ${quest.question}\n");
+      print(" ");
+      print("Options :\n");
       int qno = 0;
-      quest.options.shuffle();
       for (var opt in quest.options) {
-        print(++qno);
-        print(opt);
+        stdout.write(++qno);
+        print(" " + opt);
       }
       List<int> optAns = [];
       int numOfOpt = quest.answer.length;
@@ -36,7 +36,7 @@ class Topic {
         optAns.add(int.parse(stdin.readLineSync()!));
       }
       int j;
-      for (j = 1; j < numOfOpt; j++) {
+      for (j = 0; j < numOfOpt; j++) {
         if (quest.answer[j] != optAns[j]) break;
       }
       if (j == numOfOpt) {
@@ -54,27 +54,27 @@ class GK extends Topic {
   List<dynamic> generalKnowledge = [
     {
       "Qid": 1,
-      "question": "December 9 is celebrated as __________.",
+      "question": "December 9 is celebrated as ____.",
       "options": [
         "World Environment Day",
         "International Anti-Corruption Day",
         "World Ozone Day",
         "World Red Cross Day"
       ],
-      "answer": [1]
+      "answer": [2]
     },
     {
       "Qid": 2,
       "question": "Chandigarh is the capital of which state ?",
       "options": ["Maharashtra", "Punjab", "Haryana", "Uttarakhand"],
-      "answer": [1, 2]
+      "answer": [2, 3]
     },
     {
       "Qid": 3,
       "question":
-          "The headquarters of United Nations Environment Programme (UNEP) is located in __________.",
+          "The headquarters of United Nations Environment Programme (UNEP) is located in ____.",
       "options": ["Brussels", "Geneva", "Kathmandu", "Nairobi"],
-      "answer": [3]
+      "answer": [4]
     }
   ];
 
@@ -89,28 +89,28 @@ class DWM extends Topic {
   List<dynamic> dwm = [
     {
       "Qid": 1,
-      "question": " Data about data is called ___.",
+      "question": " Data about data is called _.",
       "options": ["Table", "Database", "Metadata", "Integration"],
-      "answer": [2]
+      "answer": [3]
     },
     {
       "Qid": 2,
       "question":
-          "The ___ software gives the user the opportunity to look at the data from a variety of different dimensions.",
+          "The _ software gives the user the opportunity to look at the data from a variety of different dimensions.",
       "options": [
         "Query tools",
         "Multidimensional Analysis Software",
         "Data Mining Tools",
         "None of the above"
       ],
-      "answer": [1]
+      "answer": [2]
     },
     {
       "Qid": 3,
       "question":
-          "The ___ operation performs a selection on one dimension of the given cube, resulting in a subcube.",
+          "The _ operation performs a selection on one dimension of the given cube, resulting in a subcube.",
       "options": ["Pivot", "Slice", "Roll-up", "Drill-down"],
-      "answer": [1]
+      "answer": [2]
     },
   ];
 
@@ -120,8 +120,3 @@ class DWM extends Topic {
     printQuest(dwmTemp);
   }
 }
-
-//git init
-//git add
-//git commit
-//git status
